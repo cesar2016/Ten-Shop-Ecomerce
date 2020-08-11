302e45ef85aa664bf80c155da9583fb3ebe2a64a
Facundo <faqqu.uri@gmail.com>
2020-08-11 20:28:55 -0300

    primera parte front

---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,128 @@
+import React, { useState } from "react"
+import "./NavBar.css"
+import SearchBar from "./SearchBar.jsx"
+
+
+export default function NavBar({ handleSubmit }) {
+    return (
+            <div>
+            <div className="NavBar">
+                <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+                    <a className="navbar-brand" href="#">Hidden brand</a>
+                    <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+                      <li className="nav-item active">
+                        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                      </li>
+                      <li className="nav-item">
+                        <a className="nav-link" href="#">Link</a>
+                      </li>
+                      <li className="nav-item">
+                        <a className="nav-link disabled" href="#">Disabled</a>
+                      </li>
+                    </ul>
+                    <SearchBar handleSubmit={handleSubmit}/>
+                    </div>
+                    </nav>
+            </div>
+            <h1>CHAUUUU</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+            <h1>HOLAAAA</h1>
+
+        </div>
+
+
+        )
+}
