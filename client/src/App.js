cb16937a01f3db10c90cded5702a7102233abbe7
Cesar2016 <cesars.pro@gmail.com>
2020-08-12 10:51:04 -0300

    Cambio Cesar From

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,29 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import NavBar from "./components/NavBar"
+import Product from "./components/Products/Product"
+ 
+
 
 function handleSubmit(e) {
     e.preventDefault()
 };
 
 function App() {
+
+     const producto = {
+         title: 'Notebook HP ',
+         precio: 3500,
+         Descr: 'sadsadasdasdas DASS '
+     }
+
+
     return (
         <div className="App jumbotron">
             <NavBar handleSubmit={handleSubmit}/>
+
+            <Product p = {producto}/>             
+            
         </div>
     );
 }
