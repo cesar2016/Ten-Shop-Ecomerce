39895eac785f14fc67b6c53ca6ed1737cd43beb6
Facundo <faqqu.uri@gmail.com>
2020-08-14 15:27:29 -0300

    facu back viernes

--- api/src/routes/categories.js
+++ api/src/routes/categories.js
@@ -11,4 +11,17 @@ server.get("/:categoria", (req, res) => {
 });
 
 
+// este post devuelve un array con dos componentes,
+// el objeto con la categoria recien publicada en la DB
+// y devuelve un booleano con true (si se agrego en la DB)
+// o devuelve un booleano con false (si no se agregó en la tabla)
+server.post("/:categoria", (req, res) => {
+  const { categoria } = req.params
+  Category.findOrCreate({ where: { name: categoria, description: "" }})
+    .then(result =>{
+      res.send(result)
+    })
+});
+
+
 module.exports = server;
--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -8,7 +8,44 @@ server.get('/', (req, res, next) => {
 			res.send(data)
 	})
  });
-   
+
+
+// este post devuelve un array con dos componentes,
+// el objeto con el producto recien publicado en la DB
+// y devuelve un booleano con true (si se agrego en la DB)
+// o devuelve un booleano con false (si no se agregó en la tabla)
+server.post("/add", (req, res) => {
+	const { name, description, price, stock, image } = req.body
+	console.log(name, description, price, stock, image)
+	Product.findOrCreate({ where: { name, description, price, stock, image } })
+		.then(product => {
+			res.send(product)
+		})
+});
+
+
+// elimina el producto por id
+// si no lo encuentra, devuelve un false
+server.delete("/:id", (req, res) => {
+	const { id } = req.params;
+	Product.destroy({ where: { id } })
+		.then(result => {
+			if (result) return res.status(200)
+			res.send(false)
+		})
+		.catch(() => res.status(404))
+});
+
+
+server.put("/:id", (req, res) => {
+	const { id } = req.params;
+	const { body } = req;
+	Product.update(body, { where: { id } })
+		.then(result => {
+			res.send(result)
+		});
+});
+
 
 
 module.exports = server;
