52ea5bd609f29ac76b74535a266ca40914795354
Cesar2016 <cesars.pro@gmail.com>
2020-08-14 10:52:25 -0300

    Nuevo fron viernes

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -11,10 +11,26 @@ export default function SearchBar({ handleSubmit }) {
 
     return (
         <div>
-            <form className="form-inline my-2 my-lg-0" onSubmit={(e) => handleSubmit(e)}>
-              <input className="form-control mr-sm-2" type="search" placeholder="..." aria-label="Search" onChange={(e) => handleChange(e)}/>
+            {/* <form className="form-inline my-2 my-lg-0" onSubmit={(e) => handleSubmit(e)}>
+              <input className="form-control subscribe-box" type="search" placeholder="Buscar" aria-label="Search" onChange={(e) => handleChange(e)}/>
               <button className="btn btn-success my-2 my-sm-0" type="submit">Buscar</button>
+            </form> */}
+
+                {/* <div class="input-group" id="subscribe">
+                    <input type="text" class="form-control subscribe-box" value="" name="subscribe" placeholder="Buscar"/>
+                    <span class="input-group-btn">
+                        <button type="button" class="btn subscribe-button"><i class="fa fa-paper-plane fa-lg"></i></button>
+                    </span>
+                </div> */}
+                
+                <div class="input-group">
+                <form className="form-inline my-4 my-lg-6" onSubmit={(e) => handleSubmit(e)}>
+                    <input className="form-control subscribe-box" type="search" placeholder="Buscar" aria-label="Search" onChange={(e) => handleChange(e)}/>
+                    <button className="btn btn-danger my-2 my-sm-0" type="submit">
+                  <span className="fa fa-search"></span>
+                </button>
             </form>
+                </div>
         </div>
     );
 };
