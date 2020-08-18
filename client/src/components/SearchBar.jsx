b08e8c6ba376e642bcb715649ad81d46db35b5f5
Cesar2016 <cesars.pro@gmail.com>
2020-08-18 00:37:58 -0300

    Nuevo Nav con menu Cat

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -20,12 +20,16 @@ export default function SearchBar({ funcionTraeDatos }) {
         <div>
 
                 <div className="input-group">
-                <form className="form-inline my-4 my-lg-6" onSubmit={(e) => handleSubmit(e)}>
+                {/* <form className="form-inline my-4 my-lg-6" onSubmit={(e) => handleSubmit(e)}>
                     <input className="form-control subscribe-box" type="search" placeholder="Buscar" aria-label="Search" onChange={(e) => handleChange(e)}/>
                     <button className="btn btn-danger my-2 my-sm-0" type="submit">
                   <span className="fa fa-search"></span>
                 </button>
-            </form>
+                    </form> */}
+                    <form class="form-inline my-2 my-lg-0" onSubmit={(e) => handleSubmit(e)}>
+                        <input style={{fontSize:"15px"}} class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" onChange={(e) => handleChange(e)}/>
+                        <button style={{fontSize:"15px"}} class="btn btn-info my-2 my-sm-0" type="submit"><i className="fa fa-search"></i></button>
+                    </form>
                 </div>
 
         </div>
