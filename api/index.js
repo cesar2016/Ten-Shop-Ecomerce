7c0a7648b60bfc5e67c75f9c964adccd2a322d4d
Facundo <faqqu.uri@gmail.com>
2020-08-21 12:05:12 -0300

    todo funcionando, antes de hacer un merge con la rama de guille

--- api/index.js
+++ api/index.js
@@ -24,7 +24,7 @@ const { conn } = require('./src/db.js');
 //false: guarda
 //True: borra
 
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
   server.listen(3001, () => {
     console.log('###### listening at 3001 ######'); // eslint-disable-line no-console
   });
