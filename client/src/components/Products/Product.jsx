81729c703ed89144733ee464acef19f355437e40
Facundo <faqqu.uri@gmail.com>
2020-08-27 10:06:37 -0300

    commit antes de traerme la rama de guille con las contraseñas cifradas

--- client/src/components/Products/Product.jsx
+++ client/src/components/Products/Product.jsx
@@ -85,9 +85,49 @@ function Product({ addCart, id, products, searchProducts, onlineUser}) {
               </div>
               <div className="puntaje">
                 <h1 id="numeroPuntaje">4.0</h1>
-                <Rater total={5} rating={3} style={{fontSize:"60px", $reactRaterLink: "red" }} onRate={onRate} />
+                <Rater total={5} rating={3.5} style={{fontSize:"60px"}} onRate={onRate} />
               </div>
             </section>
+
+            <div className="container mt-5 mb-5" >
+    <div className="d-flex justify-content-center row">
+        <div className="d-flex flex-column col-md-8">
+            <div className="d-flex flex-row align-items-center text-left comment-top p-2 bg-white border-bottom px-4">                
+            </div>
+            <div className="coment-bottom bg-white p-2 px-4">
+                <div className="d-flex flex-row add-comment-section mt-4 mb-4"><input type="text" class="form-control mr-3" placeholder="Add comment" style={{height: "40px", fontSize:"20px"}}/><button class="btn btn-primary btn-lg" type="button">Comment</button></div>
+                <div className="collapsable-comment">
+                    <div className="d-flex flex-row justify-content-between align-items-center action-collapse p-2" data-toggle="collapse" aria-expanded="false" aria-controls="collapse-1" href="#collapse-1"><span>Comments</span><i class="fa fa-chevron-down servicedrop"></i></div>
+                    <div id="collapse-1" className="collapse">
+                        <div className="commented-section mt-2">
+                            <div className="d-flex flex-row align-items-center commented-user">
+                                <h5 className="mr-2">Guillermo Ambroggio</h5><span class="dot mb-1"></span><span class="mb-1 ml-2">4 hours ago</span>
+                            </div>
+                            <div className="comment-text-sm"><span style={{fontSize:"15px"}}>Hacen envios al Chaco?</span></div>
+                            <div className="reply-section"></div>                          
+                        </div>
+                        <div className="commented-section mt-2">
+                            <div className="d-flex flex-row align-items-center commented-user">
+                                <h5 className="mr-2">Cesar Sánchez</h5><span class="dot mb-1"></span><span class="mb-1 ml-2">5 hours ago</span>
+                            </div>
+                            <div className="comment-text-sm"><span style={{fontSize:"15px"}}>Muy malo el producto, para usarlo tengo que prenderlo 2hs antes asi calienta un poco. En épocas de invierno hasta 4hs antes</span></div>
+                            <div className="reply-section">                                
+                            </div>
+                        </div>
+                        <div className="commented-section mt-2">
+                            <div className="d-flex flex-row align-items-center commented-user">
+                                <h5 className="mr-2">Makhaya andrew</h5><span class="dot mb-1"></span><span class="mb-1 ml-2">10 hours ago</span>
+                            </div>
+                            <div className="comment-text-sm"><span style={{fontSize:"15px"}}>Nunc sed id semper risus in hendrerit gravida rutrum. Non odio euismod lacinia at quis risus sed. Commodo ullamcorper a lacus vestibulum sed arcu non odio euismod. Enim facilisis gravida neque convallis a. In mollis nunc sed id. Adipiscing elit pellentesque habitant morbi tristique senectus et netus. Ultrices mi tempus imperdiet nulla malesuada pellentesque.</span></div>
+                            <div className="reply-section">                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 
 
