fdbcd42bf9353b0c49ecdefed1c2b97755e9b37d
Matias Cordoba <matiascba99@gmail.com>
2020-08-30 03:22:20 -0300

    Review mejoras

--- client/src/components/Products/Product.jsx
+++ client/src/components/Products/Product.jsx
@@ -6,15 +6,15 @@ import Swal from 'sweetalert2'
 import "./Product.css";
 import Rater from 'react-rater' // PARA INSTALAR --> npm install --save react-rater
 import 'react-rater/lib/react-rater.css';
-// import Error from "./Error"
 
-function Product({ addCart, id, products, searchProducts, onlineUser, reviews,addReview,getReviews, all_users,getUsers, newrev}) {
+function Product({ addCart, id, products, searchProducts, onlineUser, reviews,addReview,getReviews, all_users,getUsers, newrev,getOrders}) {
     const [input,setInput] = useState({});
     const [inputRating, setInputRating] = useState({});
     
     useEffect(()=> {
         getUsers();
-        getReviews(id);
+				getReviews(id);
+				getOrders("complete")
     },[newrev]);
 
     function handleInputChange (e) {
@@ -84,10 +84,11 @@ function Product({ addCart, id, products, searchProducts, onlineUser, reviews,ad
 			console.log(rating)
      setInputRating(rating)
 
-    };
-		// if (typeof onlineUser !== "object"){
-			// // return (<Error/>)
-		// } else {
+		};
+		var flag = false
+		reviews && reviews.map((p) => {if (p.userId === onlineUser.id) flag = true})
+		if (onlineUser === 0 || onlineUser === 1) {flag = true}
+		// getOrders && getOrders.map((o) => if (o.) )
     return (
        <div className="container">
 
@@ -118,7 +119,7 @@ function Product({ addCart, id, products, searchProducts, onlineUser, reviews,ad
          </section>
          <section>
           <div>
-          <h3>Product reviews</h3>              
+          <h3>Average:</h3>              
           </div>
             <div className="puntaje">
               <Rater total={5} rating={promedy(acum, reviews.length)} interactive = {false}  																								style={{fontSize:"60px"}} onRate={({rating}) => onRate(rating)} />
@@ -131,24 +132,27 @@ function Product({ addCart, id, products, searchProducts, onlineUser, reviews,ad
             <div className="d-flex flex-row align-items-center text-left comment-top p-2 bg-white border-bottom px-4">                
             </div>
             <div className="coment-bottom bg-white p-2 px-4">
-                <div style = {{height:"70px", marginBottom:"10px"}}className="d-flex flex-row add-comment-section mt-4 mb-4">
-									<div style={{width:"70%"}} >
-                 <input onChange={handleInputChange}  name = "comments" id="review" type="text" class="form-control mr-3" placeholder="Add comment" style={{height: "60px", fontSize:"20px"}}/>
-									</div>
-									<div>
-									<div style={{justifyContent:"flex-end"}}>
-										<Rater total= {5}  onRate={({rating}) => onRate(rating)} style={{fontSize:"30px", alignSelf:"flexStart", height:"30px"}}/>
-									</div>
-									<div>
-                 <button onClick={()=> handleSubmit()} class="btn btn-primary btn-lg" type="button">Leave my review.</button>
-									</div>
-                </div>
+							{!flag? 
+								<div style = {{height:"70px", marginBottom:"10px"}}className="d-flex flex-row add-comment-section mt-4 mb-4">
+								<div style={{width:"70%"}} >
+							 <input onChange={handleInputChange}  name = "comments" id="review" type="text" class="form-control mr-3" placeholder="Add comment" style={{height: "60px", fontSize:"20px"}}/>
 								</div>
+								<div>
+								<div style={{justifyContent:"flex-end"}}>
+									<Rater total= {5}  onRate={({rating}) => onRate(rating)} style={{fontSize:"30px", alignSelf:"flexStart", height:"30px"}}/>
+								</div>
+								<div>
+							 <button onClick={()=> handleSubmit()} class="btn btn-primary btn-lg" type="button">Leave my review.</button>
+								</div>
+							</div>
+							</div> : <div></div>
+							}
+                
                 <div className="collapsable-comment">
                    <div className="d-flex flex-row justify-content-between align-items-center action-collapse p-2" data-toggle="collapse" aria-expanded="false" aria-controls="collapse-1" href="#collapse-1"><span style= {{fontSize:"20px", fontWeight:"bold"}}>Reviews: </span><i class="fa fa-chevron-down servicedrop"></i></div>
                      <div id="collapse-1" className="collapse">
-                      {      reviews && reviews.map (p => 
-                        <div className="commented-section mt-2 row">
+                      {reviews && reviews.map (p => 
+                        <div className="commented-section mt-2 row border-bottom px-6">
                          <div className="d-flex flex-row align-items-center commented-user col">
                            <h2 className="mr-2">{all_users.map(u => {if( p.userId === u.id) return ("  " + u.firstname.charAt(0).toUpperCase()+u.firstname.slice(1) + " " + u.surname.charAt(0).toUpperCase()+u.surname.slice(1))})}</h2>
                            {/* <span class="dot mb-1"></span> */}
@@ -158,9 +162,9 @@ function Product({ addCart, id, products, searchProducts, onlineUser, reviews,ad
                          </div>
                          <div class="w-100"></div>
                          <div className="comment-text-sm col" style={{ textAlign:"left"}}><span style={{fontSize:"20px", textAlign:"left"}}>{p.comments}</span>
-</div>
+													</div>
                          <div className= "col" style={{marginTop:"10px", textAlign:"right"}}>                          
-                          	<span class="mb-1 ml-2" style= {{ textAlign:"right", fontSize:"14px"}}>{p.createdAt.slice(0,10)}</span>
+                          	<span class="mb-1 ml-2 " style= {{ textAlign:"right", fontSize:"14px"}}>{p.createdAt.slice(0,10)}</span>
                     		 </div>
 												</div>
               				)}
@@ -175,13 +179,13 @@ function Product({ addCart, id, products, searchProducts, onlineUser, reviews,ad
         </div>
     );
 };
-// }
 const mapDispatchToProps = dispatch => {
     return {
         addCart: (diProduc, idUser) => dispatch(addCart(diProduc, idUser)),
         addReview: (input, idProduct) => dispatch(addReview(input, idProduct)),
         getReviews: (id) => dispatch(getReviews(id)),
-        getUsers: () => dispatch(getUsers())
+				getUsers: () => dispatch(getUsers()),
+				getOrders: (status) => dispatch(getOrders(status)),
     }
   }
 
@@ -192,7 +196,8 @@ const mapDispatchToProps = dispatch => {
       onlineUser : state.onlineUser,
       reviews: state.reviews,
       all_users: state.all_users,
-      newrev: state.newrev
+			newrev: state.newrev,
+			getOrders: state.getorders
     }
   }
 
