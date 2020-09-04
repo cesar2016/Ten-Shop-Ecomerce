b2f2894ed978f1114101ed018c64121e3f5c426a
Matias Cordoba <matiascba99@gmail.com>
2020-09-04 03:17:23 -0300

    Products Home with rater

--- client/src/components/Products/TarjetCatalogue.jsx
+++ client/src/components/Products/TarjetCatalogue.jsx
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { connect } from "react-redux";
 import "./TarjetCatalogue.css"
 import { NavLink } from 'react-router-dom'
 import Swal from 'sweetalert2';
-import {addCart, lsset} from "../../actions";
+import {addCart, lsset, getReviews} from "../../actions";
+import Rater from "react-rater"
 var ls = require('local-storage');
 
 
- function Catalogo({price, name, stock, id, image, description,addCart, onlineUser, lsset}) {
-
+ function Catalogo({price, name, stock, id, image, description,addCart, onlineUser, lsset, reviews,getReviews}) {
+ 	useEffect(()=> {
+ 	 getReviews(id)
+ 	},[])
+ 	console.log("producto: ",name, "id: ", id, "reviews: ", reviews )
+ 	function promedy(acum, length){
+			var promedy = acum / length
+			if (length === 0){
+				return 0
+			}
+			promedy.toFixed(2)
+ 		  console.log("PRoemdios", promedy)
+			return promedy
+		}
+ 	var acum = 0;
+    for ( let i = 0; i < reviews.length; i++) {
+      acum = acum + reviews[i].rating;
+    }
 	function addhome(data){
 		console.log(data.target.value);
 		Swal.fire({
@@ -38,22 +55,25 @@ var ls = require('local-storage');
 		  })
 		
 	  }
-    
-    return (
+	
+	return (
 
-        <div className="content">
+		<div className="content">
 
 
-                        <div className="col-lg-3 col-md-3 col-sm-3 col-xs-12" >
+						<div className="col-lg-3 col-md-3 col-sm-3 col-xs-12" >
 							<div class="product-image-wrapper">
-                    <NavLink to ={`/product/${id}`} > 
+							<NavLink to ={`/product/${id}`} > 
 								<div class="single-products">
 										<div class="productinfo text-center">
 											<img className="ddd" src={image}/>
 											<h2>{name}</h2>
-                                            <p>$ {price}</p>
+											<p>$ {price}</p>
 											
 										</div>
+										<div style ={{"display":"flex","justifyContent": "center"}}> 
+					  		<Rater style={{'react-rater-active': 'blue'}} rating={promedy(acum,reviews.length)} interactive={false}/>
+					  	</div>
 										<div class="product-overlay">
 											<div class="overlay-content">
 												<h2>{description}</h2>
@@ -61,31 +81,36 @@ var ls = require('local-storage');
 											</div>
 										</div>
 								</div>
-                      </NavLink>
-								<div class="choose text-center">
-								<button type="button" className="btn btn-secondary addhome" onClick={(e) => addhome(e)} id="op1" value={name}><i class="fa fa-shopping-cart" style={{marginRight:'10px'}}></i>Add To Cart</button>
-                              {/*   <a href="#" class="btn btn-default"><i class="fa fa-shopping-cart"></i>Add to cart</a> */}
+					  	</NavLink>
+					  	
+								<div class=" choose text-center">
+								<button type="button" className="btn btn-secondary addhome" onClick={(e) => addhome(e)} id="op1" value={name}>
+								<i class="fa fa-shopping-cart" style={{marginRight:'10px'}}></i>
+								Add To Cart
+								</button>
+							  {/*   <a href="#" class="btn btn-default"><i class="fa fa-shopping-cart"></i>Add to cart</a> */}
 								</div>
 							</div>
 						</div>     
-                                        
+										
 
-        </div>
-    );
+		</div>
+	);
 };
 
 const mapDispatchToProps = dispatch => {
-    return {
-        addCart: (diProduc, idUser) => dispatch(addCart(diProduc, idUser)),
+	return {
+		addCart: (diProduc, idUser) => dispatch(addCart(diProduc, idUser)),
 		lsset: () => dispatch(lsset()),
-
-    }
+		getReviews: (id) => dispatch(getReviews(id))
+	}
   }
 
   const mapStateToProps = state => {
-    return {
-      onlineUser : state.onlineUser,  
-    }
+	return {
+	  onlineUser : state.onlineUser,  
+		reviews: state.reviews
+	}
   }
 
 
